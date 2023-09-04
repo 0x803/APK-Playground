d12e9d51f0c21af9af909f8fe674aec3e8c73bb7
0x803 <142042061+0x803@users.noreply.github.com>
2023-09-04 14:06:08 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,66 +1,35 @@
-// JavaScript for Pop-up and Animation
-document.addEventListener('DOMContentLoaded', function() {
-    // Show the pop-up on page load
-    const popup = document.querySelector('.popup');
-    popup.style.display = 'block';
+document.addEventListener('DOMContentLoaded', function () {
+    // Add an event listener to the form for submission
+    document.getElementById('upload-form').addEventListener('submit', function (e) {
+        e.preventDefault();
 
-    // Close the pop-up when the "Got It" button is clicked
-    const closePopupButton = document.getElementById('close-popup');
-    closePopupButton.addEventListener('click', function() {
-        popup.style.display = 'none';
-    });
-
-    // Remove the loading screen when the page is fully loaded
-    const loadingScreen = document.querySelector('.loading-screen');
-    window.addEventListener('load', function() {
-        loadingScreen.style.display = 'none';
-    });
-
-    // Theme switcher functionality
-    const themeToggle = document.getElementById('theme-toggle');
-    themeToggle.addEventListener('change', function() {
-        const body = document.body;
-        if (this.checked) {
-            body.classList.remove('light-theme');
-            body.classList.add('dark-theme');
-        } else {
-            body.classList.remove('dark-theme');
-            body.classList.add('light-theme');
+        // Reference to the file input element
+        const apkFileInput = document.getElementById('apkFile');
+        
+        // Check if a file was selected
+        if (!apkFileInput.files[0]) {
+            alert('Please select an APK file to upload.');
+            return;
         }
+
+        // Create a FormData object to send the file to the server
+        const formData = new FormData();
+        formData.append('apkFile', apkFileInput.files[0]);
+
+        // Send the form data to the server using fetch API (adjust the URL)
+        fetch('/upload', {
+            method: 'POST',
+            body: formData
+        })
+        .then(response => response.json())
+        .then(data => {
+            // Handle the server response here (e.g., show success message)
+            alert(data.message); // Display a success message
+            // You can also perform further actions based on the response
+        })
+        .catch(error => {
+            // Handle any errors (e.g., network issues)
+            console.error('Error:', error);
+        });
     });
-
-    // Handle file upload
-    const fileInput = document.getElementById('file-input');
-    const fileDropZone = document.querySelector('.file-drop-zone');
-
-    fileDropZone.addEventListener('dragover', function(e) {
-        e.preventDefault();
-        this.classList.add('drag-over');
-    });
-
-    fileDropZone.addEventListener('dragleave', function(e) {
-        e.preventDefault();
-        this.classList.remove('drag-over');
-    });
-
-    fileDropZone.addEventListener('drop', function(e) {
-        e.preventDefault();
-        this.classList.remove('drag-over');
-        const file = e.dataTransfer.files[0];
-        handleFileUpload(file);
-    });
-
-    fileInput.addEventListener('change', function(e) {
-        const file = e.target.files[0];
-        handleFileUpload(file);
-    });
-
-    function handleFileUpload(file) {
-        if (file && file.type === 'application/vnd.android.package-archive') {
-            // Handle the uploaded APK file, e.g., display it in an iframe
-            alert(`Uploaded APK file: ${file.name}`);
-        } else {
-            alert('Please upload a valid APK file.');
-        }
-    }
 });
